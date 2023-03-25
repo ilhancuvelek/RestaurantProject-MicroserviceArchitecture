f63d8a51d1023eadb617cd6bb582dc0bdfd691be
İlhan Cüvelek <cuvelekilhan@gmail.com>
2023-03-25 18:12:58 +0300

    ShoppingCartAPI Service added

---
+++ Services/Mango.Services.ShoppingCartAPI/Models/Dtos/CartDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dtos
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        
+        public virtual CartHeaderDto CartHeaderDto { get; set; }
+        public int ProductId { get; set; }
+        
+        public virtual ProductDto ProductDto { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Services/Mango.Services.ShoppingCartAPI/Models/Dtos/CartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dtos
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeaderDto { get; set; }
+        public IEnumerable<CartDetailsDto> CartDetailsDto { get; set; }
+    }
+}
