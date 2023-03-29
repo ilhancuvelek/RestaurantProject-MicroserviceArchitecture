0fca60a3918f79d751055002ae835a8162b34088
İlhan Cüvelek <cuvelekilhan@gmail.com>
2023-03-29 20:49:24 +0300

    Consume CouponAPI  Completed

--- Services/Mango.Services.ShoppingCartAPI/Repository/ICartRepository.cs
+++ Services/Mango.Services.ShoppingCartAPI/Repository/ICartRepository.cs
@@ -8,5 +8,9 @@ namespace Mango.Services.ShoppingCartAPI.Repository
         Task<CartDto> CreateUpdateCart(CartDto cartDto);
         Task<bool> RemoveFromCart(int cartDetailsId);
         Task<bool> ClearCart(string userId);
+
+        //coupon
+        Task<bool> ApplyCoupon(string userId, string couponCode);
+        Task<bool> RemoveCoupon(string userId);
     }
 }
